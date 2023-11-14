3a0dc1caf57ac19eb0207b29c5de97b5f22e9e04
Adam <54817317+macHimself@users.noreply.github.com>
2023-11-14 17:27:44 +0100

    some small update

--- algorithms.playground/Contents.swift
+++ algorithms.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 
 let clock = ContinuousClock()
 var array: [Int] = []
-for _ in 0...10000{//10000{
+for _ in 0...10{//10000{
     array.append(Int.random(in: 1..<1000))
 }
 
@@ -114,7 +114,6 @@ func shakeSort () {
 
 func selectionSort () {
     var answer = array
-    print(answer)
     for index in 0...answer.count-2 {
         var first = index
         for jindex in index+1...answer.count-1 {
@@ -126,10 +125,21 @@ func selectionSort () {
         answer[first] = answer[index]
         answer[index] = x
     }
-    print(answer)
 }
 
 
 //let result = clock.measure(selectionSort)
 //print(result, array.count)
 //print("done")
+
+func quickSort() {
+    var answer = array
+    print(answer)
+    
+    
+    print(answer)
+}
+
+let result = clock.measure(selectionSort)
+print(result, array.count)
+print("done")
