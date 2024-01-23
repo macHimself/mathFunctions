4b42a343cccae645d286c37ef86e2a23211c31e8
Adam <54817317+macHimself@users.noreply.github.com>
2024-01-23 20:17:54 +0100

    bucket sort

--- algorithms.playground/Contents.swift
+++ algorithms.playground/Contents.swift
@@ -3,7 +3,7 @@ import Foundation
 let clock = ContinuousClock()
 var array: [Int] = []
 for _ in 0...10000{//10000{
-    array.append(Int.random(in: 1..<1000))
+    array.append(Int.random(in: 1..<10000))
 }
 
 func lichSud() {
@@ -372,6 +372,36 @@ func mergeSort () {
 
 
 
-let result = clock.measure(mergeSort)
+//let result = clock.measure(mergeSort)
+//print(array,"\n",result,"\n", array.count)
+//print("done")
+
+func bucketSort() {
+    array
+    let n = array.count-1
+    let c = 25
+    let cc = 10000
+    var C: [Int] = []
+    var B: [Int] = [Int](repeating: 0, count: cc+1)
+    
+    for _ in 0...(cc/c)-1 {
+        C.append(0)
+    }
+    for i in 0...n {
+        let j = array[i]/c
+        C[j] += 1
+    }
+    for i in 1...(cc/c)-1 {
+        C[i] = C[i] + C[i-1]
+    }
+    for i in stride(from: n, to: 0, by: -1) {
+        let j = array[i] / c
+        C[j] = C[j] - 1
+        B[C[j]] = array[i]
+    }
+    array = B
+}
+
+let result = clock.measure(bucketSort)
 print(array,"\n",result,"\n", array.count)
 print("done")
